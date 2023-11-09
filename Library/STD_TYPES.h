f07a758d0ef493e2e32ede77d134c5c8ae2dee81
khaledsoltan95 <kh.soltan95@gmail.com>
2023-11-09 22:54:06 +0200

    Adding unsigned long long int (uint64) to STD_TYPES.h

--- Library/STD_TYPES.h
+++ Library/STD_TYPES.h
@@ -5,6 +5,7 @@ typedef signed char sint8 ;
 typedef unsigned short int uint16 ;
 typedef signed short int sint16 ;
 typedef unsigned long int uint32 ;
+typedef unsigned long long int uint64 ;
 typedef signed long int sint32 ;
 typedef float f32 ;
 typedef double f64 ;
